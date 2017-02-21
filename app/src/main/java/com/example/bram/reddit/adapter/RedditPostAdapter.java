2ca423658de2910f1e58465ef5f6cbc186cd4767
Bram Hoendervangers <bram@Brams-MacBook-Pro.local>
2017-02-21 17:30:29 +0700

    Added loading indicator and endless scrolling

--- app/src/main/java/com/example/bram/reddit/adapter/RedditPostAdapter.java
+++ app/src/main/java/com/example/bram/reddit/adapter/RedditPostAdapter.java
@@ -13,6 +13,7 @@ import com.example.bram.reddit.R;
 import com.example.bram.reddit.api.model.RedditPost;
 import com.example.bram.reddit.util.Util;
 import com.squareup.picasso.Picasso;
+import com.wang.avi.AVLoadingIndicatorView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +25,10 @@ import butterknife.ButterKnife;
  * Created by bram on 2/21/17.
  */
 
-public class RedditPostAdapter extends RecyclerView.Adapter<RedditPostAdapter.ViewHolder> {
+public class RedditPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final int VIEW_TYPE_LOADING = 0;
+    public static final int VIEW_TYPE_DATA = 1;
+
     private final List<RedditPost> postList = new ArrayList<>();
     private final Context context;
 
@@ -33,44 +37,74 @@ public class RedditPostAdapter extends RecyclerView.Adapter<RedditPostAdapter.Vi
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.reddit_post_item, parent, false);
-        return new ViewHolder(view);
+    public int getItemViewType(int position) {
+        if (position >= postList.size()) {
+            return VIEW_TYPE_LOADING;
+        }
+
+        return VIEW_TYPE_DATA;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        RedditPost redditPost = postList.get(position);
-        holder.authorTextView.setText(redditPost.getAuthor());
-        holder.descriptionTextView.setText(redditPost.getTitle());
-        holder.commentsNumberTextView.setText(redditPost.getNumComments() + " comments");
-        holder.createdTextView.setText(Util.formatDate(redditPost.getCreated()));
-        
-        Picasso.with(context)
-                .load(redditPost.getThumbnail())
-                .into(holder.tumbnailImageView);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DATA) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.reddit_post_item, parent, false);
+            return new PostViewHolder(view);
+        }
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.loading_indicator, parent, false);
+        return new LoadingViewHolder(view);
     }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof PostViewHolder) {
+            PostViewHolder postViewHolder = (PostViewHolder) holder;
+            RedditPost redditPost = postList.get(position);
+            postViewHolder.authorTextView.setText(redditPost.getAuthor());
+            postViewHolder.descriptionTextView.setText(redditPost.getTitle());
+            postViewHolder.commentsNumberTextView.setText(redditPost.getNumComments() + " comments");
+            postViewHolder.createdTextView.setText(Util.formatDate(redditPost.getCreated()));
+
+            Picasso.with(context)
+                    .load(redditPost.getThumbnail())
+                    .into(postViewHolder.tumbnailImageView);
+        } else {
+            ((LoadingViewHolder)holder).loadingIndicator.show();
+            
+        }
+    }
+
 
     @Override
     public int getItemCount() {
-        return postList.size();
+        return postList.size() + 1;
     }
 
-    public void addPosts(List<RedditPost> posts){ 
-        int startPos = posts.size()-1;
+    public void addPosts(List<RedditPost> posts) {
         postList.addAll(posts);
-        notifyItemRangeChanged(startPos, posts.size());
+        notifyDataSetChanged();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class LoadingViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.loading_indicator_view) AVLoadingIndicatorView loadingIndicator;
+        
+        public LoadingViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.tv_post_author) TextView authorTextView;
         @BindView(R.id.tv_post_description) TextView descriptionTextView;
         @BindView(R.id.iv_post_thumbnail) ImageView tumbnailImageView;
         @BindView(R.id.tv_post_comments_number) TextView commentsNumberTextView;
         @BindView(R.id.tv_post_created) TextView createdTextView;
-        
-        public ViewHolder(View itemView) {
+
+        public PostViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
--- app/src/main/java/com/example/bram/reddit/ui/MainActivity.java
+++ app/src/main/java/com/example/bram/reddit/ui/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.rv_reddit_post_list) RecyclerView redditPostsRecycleView;
     private RedditPostAdapter redditPostsAdapter;
+    private RedditListing lastRedditListing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +29,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        
+
 //        redditPostsRecycleView.setHasFixedSize(true);
-        redditPostsRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        redditPostsRecycleView.setLayoutManager(layoutManager);
         redditPostsAdapter = new RedditPostAdapter(this);
         redditPostsRecycleView.setAdapter(redditPostsAdapter);
-        
+
+        redditPostsRecycleView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+        redditPostsRecycleView.addOnScrollListener(new EndlessScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                if (lastRedditListing != null) {
+                    loadRedditPosts(null, lastRedditListing.getAfter());
+                } else {
+                    loadRedditPosts(null,null);
+                }
+            }
+        });
+    } 
+
+    private void loadRedditPosts(String before, String after) {
         RedditService.INSTANCE.getRedditApi().getTop(null, null).enqueue(new Callback<RedditListing>() {
             @Override
             public void onResponse(Call<RedditListing> call, Response<RedditListing> response) {
                 if (response.isSuccessful()) {
-                    redditPostsAdapter.addPosts(response.body().getPosts());
+                    lastRedditListing = response.body();
+                    redditPostsAdapter.addPosts(lastRedditListing.getPosts());
                     redditPostsAdapter.notifyDataSetChanged();
                 }
             }
 
             @Override
             public void onFailure(Call<RedditListing> call, Throwable t) {
-                t.printStackTrace();
+                Log.d("Load failure", t.getMessage());
             }
         });
     }
