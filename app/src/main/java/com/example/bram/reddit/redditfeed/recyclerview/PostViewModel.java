6ea1ca2659b8a5ad28b4c6ed2b52f11e2289acd5
Bram Hoendervangers <bram@Brams-MacBook-Pro.local>
2017-02-23 12:23:23 +0700

    Don't tightly couple with view holder

--- app/src/main/java/com/example/bram/reddit/redditfeed/recyclerview/PostViewModel.java
+++ app/src/main/java/com/example/bram/reddit/redditfeed/recyclerview/PostViewModel.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.example.bram.reddit.api.model.RedditPost;
+import com.example.bram.reddit.lib.MyView;
 import com.example.bram.reddit.lib.ViewModel;
 import com.squareup.picasso.Picasso;
 
@@ -12,26 +13,9 @@ import com.squareup.picasso.Picasso;
  * Created by bram on 2/22/17.
  */
 
-public class PostViewModel extends ViewModel<PostViewHolder> {
+public class PostViewModel extends ViewModel<MyView> {
     private RedditPost redditPost;
-
-    @BindingAdapter({"bind:imageUrl"})
-    public static void loadImage(ImageView view, String imageUrl) {
-        Picasso.with(view.getContext())
-                .load(imageUrl)
-                .into(view);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle bundle) {
-
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-
-    }
-
+    
     public RedditPost getRedditPost() {
         return redditPost;
     }
@@ -40,4 +24,12 @@ public class PostViewModel extends ViewModel<PostViewHolder> {
         this.redditPost = redditPost;
         notifyChange();
     }
+    
+    @BindingAdapter({"bind:imageUrl"})
+    public static void loadImage(ImageView view, String imageUrl) {
+        Picasso.with(view.getContext())
+                .load(imageUrl)
+                .into(view);
+    }
+
 }
